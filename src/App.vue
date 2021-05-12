64ef0bf423a9d7f8d9aa7905b49d41d005867c5a
yeasin_ahammed_apon <yeasinahammedapon@gmail.com>
2021-05-13 01:50:23 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Part-1/>
+    <h1>my name is apon</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Part1 from './components/part1'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   Part1
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/part1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h1>part 1 is here</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
